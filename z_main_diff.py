44c00957f19373147f69e98d54d37410b29d1c5c
hetima <33700+hetima@users.noreply.github.com>
2023-06-13 11:09:47 +0900

    init

---
+++ z_main_diff.py
@@ -0,0 +1,100 @@
+# pip install inquirerpy
+
+import os
+import sys
+import glob
+from InquirerPy import inquirer
+# from InquirerPy.validator import NumberValidator
+import subprocess
+# import argparse
+
+MODELS_PATH = "exp"
+DDSP_MODEL_DIR = "combsub"
+DIFF_MODEL_DIR = "diff"
+PYTHON_PATH = os.path.join(os.environ["VIRTUAL_ENV"], "Scripts/python.exe")
+
+def exec_main_diff(ddsp_model, diff_model, input_path, output_path):
+    args = [PYTHON_PATH, "main_diff.py"]
+    args.extend(["-i", input_path])
+    args.extend(["-ddsp", ddsp_model])
+    args.extend(["-diff", diff_model])
+    args.extend(["-o", output_path])
+    # 追加の引数
+    args.extend(["-kstep", "300"])
+    subprocess.run(args)
+
+def unique_ouput_path(file_path):
+    if not os.path.exists(file_path):
+        return file_path
+    file_path_splitext = os.path.splitext(file_path)[0]
+    for i in range(2000):
+        candidate_path = file_path_splitext + "-" + str(i+1) + ".wav"
+        if not os.path.exists(candidate_path):
+            return candidate_path
+    return None
+
+def latest_checkpoint_path(dir_path, regex="model_*.pt"):
+    f_list = glob.glob(os.path.join(dir_path, regex))
+    f_list.sort(key=lambda f: int("".join(filter(str.isdigit, f))))
+    if len(f_list) <= 0:
+        return ""
+    x = f_list[-1]
+    return x
+
+def select_project():
+    dir_path = MODELS_PATH
+    dirs = []
+    for f in os.listdir(dir_path):
+        if os.path.isdir(os.path.join(dir_path, f)):
+            if os.path.exists(os.path.join(dir_path, f, DDSP_MODEL_DIR)) and os.path.exists(os.path.join(dir_path, f, DIFF_MODEL_DIR)):
+                dirs.append(f)
+    slct = inquirer.select(message="Select model:", choices=dirs, multiselect=False).execute()
+    return slct
+
+def models_in_project(project_path):
+    ddsp_path = os.path.join(project_path, DDSP_MODEL_DIR)
+    diff_path = os.path.join(project_path, DIFF_MODEL_DIR)
+    return latest_checkpoint_path(ddsp_path), latest_checkpoint_path(diff_path)
+
+def do_main():
+    print(sys.base_prefix)
+    print(PYTHON_PATH)
+    #model
+    project = select_project()
+    if project == "":
+        print("model is empty")
+        exit(0)
+    project_path = os.path.join(MODELS_PATH, project)
+    ddsp_path, diff_path = models_in_project(project_path)
+    if not ddsp_path:
+        print("ddsp model not found")
+        exit(0)
+    if not diff_path:
+        print("diff model not found")
+        exit(0)
+    print(ddsp_path)
+    print(diff_path)
+
+    #input file
+    input_path = inquirer.filepath(message="wav file path:").execute()
+    if not input_path:
+        exit(0)
+    if input_path[0] == '"' or input_path[0] == "'":
+        input_path = input_path[1:-1]
+    if not os.path.exists(input_path):
+        print("file not found")
+        exit(0)
+    #output file
+    input_name = os.path.basename(input_path)
+    output_name = project + "-" + input_name
+    output_path = os.path.join(os.path.dirname(input_path), output_name)
+    output_path = unique_ouput_path(output_path)
+    if not output_path:
+        print("output path confused")
+        exit(0)
+
+    print(output_path)
+    exec_main_diff(ddsp_path, diff_path, input_path, output_path)
+
+if __name__ == '__main__':
+    do_main()
